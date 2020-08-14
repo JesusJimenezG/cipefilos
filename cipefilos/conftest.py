80867526bb2679db274e45a78055fcbe6bede31e
Jesus <jesusalfonsojg7@gmail.com>
2020-08-14 04:30:41 -0400

    Inicializado el proyecto con cookiecutter

---
+++ cipefilos/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cipefilos.users.models import User
+from cipefilos.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
